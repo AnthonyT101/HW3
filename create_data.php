93c9adbb3749bfd740581c4c6f057c34a2820d3c
AnthonyT101 <94636228+AnthonyT101@users.noreply.github.com>
2023-09-12 13:54:54 -0400

    Create create_data.php

---
+++ create_data.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<!-- Author: Anthony, Malachi, Sami
+Date: 9/11/2023
+File: Homework3
+Purpose: Homework #3
+-->
+
+<html>
+	<head>
+		<title> PHP HW3</title>
+		<link rel="stylesheet" type="text/css" href="sample.css">
+	</head>
+	<body style="background-color: gray;">
+		<center><h1>Data processed.</h1></center>
+	<?php
+		
+	?>
+	
+	<br>
+	<center><a href="html.html"> Return back to original page</a></center>
+	</body>
+</html>
